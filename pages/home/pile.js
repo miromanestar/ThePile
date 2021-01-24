317c48fe96647c9e854cf5f2cddc5a3c0e870483
Miro Manestar <miroimanestar@gmail.com>
2021-01-24 04:33:17 -0500

    It's useful now

--- pages/home/pile.js
+++ pages/home/pile.js
@@ -9,9 +9,7 @@ firebase.auth().onAuthStateChanged( function(user) {
 
         //If the user can read from the user database, it means they're an authorized user
         firebase.database().ref(`/users/${ user.uid }`).once('value').then( (data) => {
-            if (data.val().role !== 'viewer')
-                $('.pile-controls').show();
-            loadPile();
+            loadPile(data.val().role);
         }).catch( (error) => {
             $('#pile-area').hide();
             $('#pile-area').after(`
@@ -24,48 +22,167 @@ firebase.auth().onAuthStateChanged( function(user) {
     }
 });
 
-function loadPile() {
+function loadPile(role) {
     let pileRef = firebase.database().ref('/pile/'); //.limitToLast(???) ???
 
     //Initially add all the pile items/add a pile item each time it's added in real time to all clients
     pileRef.on('child_added', (data) => {
-        $('pile-area-empty').remove();
+        $('.pile-area-empty').remove();
+        if (role !== 'viewer') {
+            $('.pile-controls').show();
+            $('#no-item-add-btn').remove();
+        }
+
+        displayItem(data.key, data.val());
     });
 
-    $('#pile-loader').hide(); //Remove the load indicator
+    $('#pile-loader').remove(); //Remove the load indicator
 
     //Display a message if no pile items could be retrieved/don't exist
     setTimeout(function() {
-        noItems();
+        noItems(role);
     }, 100);
 
     //Remove associated element if a pile item is deleted
     pileRef.on('child_removed', (data) => {
-
-
-        noItems(); //If there are zero elements, display a message saying as such
+        $(`#pile-item_${ data.key }`).fadeOut('fast', function() { 
+            $(this).remove();
+            noItems(role); //If there are zero elements, display a message saying as such
+        });
     });
 
     //Update associated element if it's entry changed in the database in real time
     pileRef.on('child_changed', (data) => {
-
+        displayItem(data.key, data.val());
+        //Also implement something for edit history here
     });
 }
 
-function noItems() {
-    if ($('.pile-item').length === 0)
+//Will add/modify items from the pile in real time
+function displayItem(id, item, alreadyExists) {
+    const itemHTML = `
+    <div id="pile-item_${ id }" class="pile-item">
+        <div class="pile-item-content">
+            <div class="pile-item-header">
+                <span class="pile-item-name display-4 mr-2">${ item.name }</span>
+                <span class="pile-item-user text-muted">By ${ item.user }</span>
+            </div>
+            <div class="pile-item-desc mt-3">${ item.desc }</div>
+
+            <div class="pile-item-btns mt-1">
+                <button type="button" class="edit-item-btn btn font-weight-bold btn-primary ml-2" onclick="edit('${ id }');">Edit</button>
+                ${ item.history ?
+                `<button type="button" class="edit-item-btn btn font-weight-bold btn-primary ml-2 disabled" onclick="alert('Not Implemented');">History</button>`
+                : '' }
+                <button type="button" class="delete-item-btn btn font-weight-bold btn-danger ml-2" onclick="deleteSingle('${ id }');">Delete</button>
+            </div>
+        </div>
+        <div class="pile-item-time text-muted d-flex justify-content-end"> ${ item.history ? '<span class="dot dot-warning mr-2"></span>' : '' }${ epochToDate(item.time) } at ${ epochToTime(item.time) }</div>
+    </div>
+    `;
+
+    if (!alreadyExists) {
+        $('#pile-items').append(itemHTML);
+        $('#pile-area').animate({
+            scrollTop: $('#pile-area').get(0).scrollHeight
+        }, 0);
+    } else {
+        $(`#pile-item_${ id }`).replaceWith(itemHTML);
+    }
+}
+
+function add(formData) {
+    $('#pile-modal').fadeOut('fast');
+
+    firebase.database().ref(`/pile/`).push({
+        'uid': firebase.auth().currentUser.uid,
+        'user': firebase.auth().currentUser.displayName,
+        'time': Date.now(),
+        'name': formData[0].value,
+        'desc': formData[1].value
+    }).catch(function (error) {
+        alert('You do not have permission to add items to the pile scrub.');
+    });
+}
+
+function edit(id) {
+    $('#pile-form button[type=submit]').attr('onclick', `pushEdit('${ id }', $('#pile-form').serializeArray())`);
+    $('#pile-form button[type=submit]').html('Update');
+    $('#pile-modal').fadeIn('fast');
+}
+
+function pushEdit(id, formData) {
+    $('#pile-form button[type=submit]').attr('onclick', `pushEdit('${ id }', add($('#pile-form').serializeArray());`);
+
+    firebase.database().ref(`/pile/${ id }`).once('value').then( (data) => {
+        firebase.database().ref(`/pile/${ id }/history`).push(data.val()); //Add current version to history of item
+        firebase.database().ref(`/pile/${ id }`).update({
+            'uid': firebase.auth().currentUser.uid,
+            'user': firebase.auth().currentUser.displayName,
+            'time': Date.now(),
+            'name': formData[0].value,
+            'desc': formData[1].value,
+        });
+    });
+}
+
+function deleteSingle(id) {
+    if (!confirm('Are you sure you want to delete this item?'))
+        return;
+    
+    firebase.database().ref(`/pile/${ id }`).remove();
+}
+
+function reorder() {
+    alert('Not yet implemented');
+}
+
+function noItems(role) {
+    if ($('.pile-item').length === 0) {
+        $('.pile-controls').hide();
         $('#pile-area').append(`
             <div class="pile-area-empty py-3 text-muted text-center">
             Looks like there isn't anything on the pile!
             <br />
             Add something you pink kebab-horned sea unicorn!</div>
-        `); 
+        `);
+        
+        if (role != 'viewer')
+            $('#pile-area').append(`
+                <button type="button" id="no-item-add-btn" class="btn btn-primary font-weight-bold mx-auto d-block" onclick="$('#pile-modal').fadeIn('fast');">Add</button>
+            `);
+    }
 }
 
-function add() {
-    alert('Not implemented');
+function epochToTime(s) {
+    date = new Date(s);
+
+    minute = date.getMinutes();
+    hour = date.getHours();
+
+    let AMPM = '';
+    if (hour === 0) {
+        hour = 12;
+        AMPM = 'AM';
+    } else if (hour < 12) {
+        AMPM = 'AM';
+    } else if (hour >= 12) {
+        if (hour !== 12)
+            hour = hour - 12;
+        AMPM = 'PM';
+    }
+
+    if (minute < 10)
+        minute = '0' + minute;
+
+    return `${ hour }:${ minute } ${ AMPM }`;
 }
 
-function edit() {
-    alert('Not implemented');
+function epochToDate(s) {
+    date = new Date(s);
+    year = date.getFullYear();
+    month = date.toLocaleString('default', { month: 'long' });
+    day = date.getDate();
+
+    return `${ month } ${ day }, ${ year }`;
 }
